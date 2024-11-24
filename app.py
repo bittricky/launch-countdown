c5766848f07aff3de7f187e256001c707ad612dc
Mitul Patel <me@mitulpa.tel>
2024-11-24 15:13:24 -0500

    feat(404): added a 404 page

--- app.py
+++ app.py
@@ -28,5 +28,9 @@ def countdown():
     else:
         return redirect(url_for('index'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
